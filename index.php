418139fa7a81478b7cffccfd98b49c86e0bca537
Hein Htet Aung <heinjame@gmail.com>
2019-04-27 22:25:32 +0630

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	</style>
 <body>
 <center>
-<h1 style="font-family:PsycheWard-Regular ">Beginner Lab</h1>
+<h1 style="font-family:PsycheWard-Regular ">WAB's Beginner Lab</h1>
 <h1 style="color:black;font-family: GistLight">Practice XSS</h1>
 <a href="xss/level1.php">level1</a>
 <a href="xss/level2.php">level2</a>
@@ -25,6 +25,10 @@
 <a href="sqli/level4.php">level4</a>
 <a href="sqli/level5.php">level5</a>
 <a href="sqli/level6.php">level6</a>
+<h1 style="font-family:GistLight">Playing With Local Files</h1>
+<a href="fileinclusion/level1.php">level1</a>
+<a href="fileinclusion/level2.php">level2</a>
+<a href="fileinclusion/level3.php">level3</a>
 <div style="margin-top: 50px;color: #d61111">
 	&copy; <?php echo date ('Y'); ?> Developed by W.A.B. All rights reserved
 </div>
